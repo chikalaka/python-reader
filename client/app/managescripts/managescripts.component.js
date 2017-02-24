9149d2de49349a162051e2ed192a34629f70d5ac
chikalaka <edan.chetrit@gmail.com>
2017-02-24 11:53:11 +0200

    added link in navbar, added CRD to controller

--- client/app/managescripts/managescripts.component.js
+++ client/app/managescripts/managescripts.component.js
@@ -6,9 +6,49 @@ const uiRouter = require('angular-ui-router');
 import routes from './managescripts.routes';
 
 export class ManagescriptsComponent {
+
+  scripts = [];
+  newScript = {scriptName: '', scriptCode: '', isWorking: 'true'};
+
   /*@ngInject*/
-  constructor() {
-    this.message = 'Hello';
+  constructor($http) {
+    this.$http = $http;
+  }
+
+  $onInit(){
+    this.$http.get('/api/scripts')
+      .then(response => {
+        this.scripts = response.data;
+        console.log(this.scripts);
+      })
+      .catch(error => {
+        console.log('error: ', error);
+      });
+  }
+
+  addScript(){
+    this.$http.post('/api/scripts', this.newScript)
+    .then((response) => {
+      this.newScript = {scriptName: '', scriptCode: '', isWorking: 'true'};
+    })
+    .catch(error => {
+      console.log('error: ', error);
+    });
+  }
+
+  removeScript(){
+    let selectedId = this.removeScriptSelection;
+    this.$http.delete('/api/scripts/' + selectedId)
+    .then(response => {
+      console.log('removeScript(), script successfuly deleted');
+    })
+    .catch(error => {
+      console.log('error: ', error);
+    });
+  }
+
+  executeScript(){
+    console.log(this.executeScriptSelection);
   }
 }
 
@@ -16,7 +56,6 @@ export default angular.module('pythonReaderApp.managescripts', [uiRouter])
   .config(routes)
   .component('managescripts', {
     template: require('./managescripts.html'),
-    controller: ManagescriptsComponent,
-    controllerAs: 'managescriptsCtrl'
+    controller: ManagescriptsComponent
   })
   .name;
